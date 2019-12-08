bb4f9c3bd01bcb1f6effad6e77c15376fc05d937
Adam Balassa <balassaadi@gmail.com>
2019-12-08 12:54:57 +0100

    radar passes speed test

--- Virtual/src/hu/bme/aut/fox/robotvacuum/virtual/components/ContinuousRadarTest.java
+++ Virtual/src/hu/bme/aut/fox/robotvacuum/virtual/components/ContinuousRadarTest.java
@@ -11,16 +11,17 @@ class ContinuousRadarTest {
 		Simulation simulation = new Simulation();
 		ContinuousRadar radar = simulation.getRadar();
 		Vec2 point = radar.intersectWithLine(new Vec2(1, 2), new Vec2(3.1, 6.2), new Vec2(0, 3), Math.PI / 4);
-		assertEquals( 3, (int)point.getX());
-		assertEquals( 6, (int)point.getY());
-		assertTrue(Math.abs(3 - point.getX()) < 0.00001);
-		assertTrue(Math.abs(6 - point.getY()) < 0.00001);
+		assertTrue(Math.abs(3 - point.getX()) < 0.00001, "x is " + point.getX() + " expected: " + 3);
+		assertTrue(Math.abs(6 - point.getY()) < 0.00001, "y is " + point.getY() + " expected: " + 6);
 	}
 
+	@Test
 	void testRadarSpeed() {
 		Simulation simulation = new Simulation();
 		ContinuousRadar radar = simulation.getRadar();
 		long t1 = System.currentTimeMillis();
 		radar.getRadarData();
+		long t2 = System.currentTimeMillis();
+		assertTrue(t2 - t1 < 20, "Processing data took " + (t2 - t1) / 1000 + " seconds");
 	}
 }
